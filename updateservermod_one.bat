f131d386f3ed918b39ee442da3238ffaefb1e732
jakubluc <107751484+jakubluc@users.noreply.github.com>
2022-10-22 21:11:12 +0200

    full force forvard

--- updateservermod_one.bat
+++ updateservermod_one.bat
@@ -1,5 +1,4 @@
-#!/bin/batch
 set number_of_mod=%1
 :: rm -r -f ~/Steam/steamapps/workshop/content/602960/$number_of_mod
-steamcmd.exe +login anonymous +workshop_download_item 602960 %number_of_mod% +quit
+steamcmd.exe +force_install_dir "steamdir" +login anonymous +workshop_download_item 602960 %number_of_mod% +quit
 timeout /T 1
--- updateservermods.bat
+++ updateservermods.bat
@@ -1,18 +1,20 @@
-#!/bin/batch
 :: ./backupConfigs.sh :: todo
 :: idk if echo will work check it
+set barotrauma_dir=%1
 echo "Updating following mods:"
-bin\grep.exe -o -P "(?<=LocalMods\/)(.*?)(?=\/filelist\.xml)" config_player.xml > bufferscript
+"bin\grep.exe" -o -P "(?<=LocalMods\/)(.*?)(?=\/filelist\.xml)" barotrauma_dir\config_player.xml > bufferscript
 type bufferscript
 echo ""
-@echo off
-for /f "tokens=*" %%a in (bufferscript.txt) do (
-  updateservermod_one.bat  %%a
+mkdir "steamdir"
+for /f "tokens=*" %%a in (bufferscript) do (
+  "C:\Users\Mason\Desktop\Masons Server\updateservermod_one.bat" %%a
 )
 pause
 echo ""
 echo ""
 echo "All queued Mods have been updated"
-delete bufferscript
+del bufferscript
+::stuff that copies updated most from source to LocalMods goes here
+::rmdir /s "steamdir"
 :: ./restoreConfigsFromBackup.sh :: todo
 echo "Done updating mods!"
