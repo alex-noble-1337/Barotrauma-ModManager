41439582d1a97f8054d7c6d6d06ff9c5ebbe62e8
jakubluc <107751484+jakubluc@users.noreply.github.com>
2022-10-22 18:53:57 +0200

    123

--- configserver.sh
+++
@@ -1,3 +0,0 @@
-cp -n ~/Steam/steamapps/common/Barotrauma\ Dedicated\ Server/serversettings.xml serversettings.xml
-nano serversettings.xml
-cp serversettings.xml ~/Steam/steamapps/common/Barotrauma\ Dedicated\ Server/serversettings.xml 
--- runserver.sh
+++
@@ -1,3 +0,0 @@
-./closeserver.sh
-cd ~/Steam/steamapps/common/Barotrauma\ Dedicated\ Server/
-screen -S server -dm  "./DedicatedServer.exe"
