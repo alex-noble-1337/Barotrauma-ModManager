41ef895b7a962d25484541e2e2a812d3ad5e93ba
Milord <100714051+Milord-ThatOneModder@users.noreply.github.com>
2023-03-28 00:33:04 +0200

    Update download_script_exmple.sh

--- download_script_exmple.sh
+++ download_script_exmple.sh
@@ -3,7 +3,7 @@
 echo "UPDATING MOD MANAGER"
 wget -N https://github.com/Milord-ThatOneModder/Barotrauma-ModManager/releases/latest/download/ModManager.zip
 unzip -qo ModManager.zip
+pip install requests
 echo -e "\n\n"
 echo "RUNNING MOD MANAGER"
-pip install requests
 python3 ModManager/ModManager.py -s "steamcmd/steamcmd.sh" -t "ModManager"
